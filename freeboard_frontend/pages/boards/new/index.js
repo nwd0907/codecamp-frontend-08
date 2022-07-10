341bc24f8a1f1fa5c0e1d8d8e708f86e3f6cfa5d
nwd0907 <nwd0907@newbizstart.com>
2022-07-10 21:29:34 +0900

    Update boardDetail

--- freeboard_frontend/pages/boards/new/index.js
+++ freeboard_frontend/pages/boards/new/index.js
@@ -24,6 +24,7 @@ import {
   ZipcodeWrapper
 } from "../../../styles/emotion";
 import { gql, useMutation } from '@apollo/client'
+import { useRouter } from 'next/router'
 
 const CREATE_BOARD = gql`
   mutation createBoard($createBoardInput: CreateBoardInput!){
@@ -34,6 +35,8 @@ const CREATE_BOARD = gql`
 `
 
 export default function BoardWriteUI() {
+  const router = useRouter()
+
   const [writer, setWriter] = useState("");
   const [password, setPassword] = useState("");
   const [title, setTitle] = useState("");
@@ -88,17 +91,22 @@ export default function BoardWriteUI() {
       setContentsError("내용을 입력해주세요.");
     }
     if (writer && password && title && contents) {
-      const result = await createBoard({
-        variables: {
-          createBoardInput: {
-            writer: writer,
-            password: password,
-            title: title,
-            contents: contents
+      try {
+        const result = await createBoard({
+          variables: {
+            createBoardInput: {
+              writer: writer,
+              password: password,
+              title: title,
+              contents: contents
+            }
           }
-        }
-      })
-      console.log(result)
+        })
+        console.log(result.data.createBoard._id)
+        router.push(`/boards/${result.data.createBoard._id}`)
+      } catch(error) {
+        alert(error.message)
+      }
     }
   };
 

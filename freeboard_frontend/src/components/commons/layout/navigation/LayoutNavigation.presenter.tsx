992120bef67501b80a5be24bc20e75dddabfde8c
nwd0907 <nwd0907@newbizstart.com>
2022-07-21 03:33:17 +0900

    Update layout

--- freeboard_frontend/src/components/commons/layout/navigation/LayoutNavigation.presenter.tsx
+++ freeboard_frontend/src/components/commons/layout/navigation/LayoutNavigation.presenter.tsx
@@ -3,8 +3,6 @@ import { MenuItem, Wrapper } from "./LayoutNavigation.styles";
 import { ILayoutNavigationUIProps } from "./LayoutNavigation.types";
 
 const NAVIGATION_MENUS = [
-  { name: "나의파이어베이스", page: "/myfirebase" },
-  { name: "라이브강아지", page: "/openapis" },
   { name: "라이브게시판", page: "/boards" },
   { name: "라이브상품", page: "/markets" },
   { name: "마이페이지", page: "/mypages" },
